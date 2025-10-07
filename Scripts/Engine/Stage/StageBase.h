3064d183f7df28c815ec8327f8c5025a686c27e0
ichikawakuu <mcd2476004@stu.o-hara.ac.jp>
2025-10-07 16:55:34 +0900

    Add StageBase

---
+++ Scripts/Engine/Stage/StageBase.h
@@ -0,0 +1,24 @@
+/*
+ *	@file	StageBase.h
+ */
+
+#ifndef _STAGEBASE_H_
+#define _STAGEBASE_H_
+
+class StageBase {
+private:
+
+
+public:
+	StageBase() = default;
+	~StageBase() = default;
+
+public:
+	// •Ğ•t‚¯ˆ—
+	void 
+
+	// XVˆ—
+	
+};
+
+#endif // !_STAGEBASE_H_
