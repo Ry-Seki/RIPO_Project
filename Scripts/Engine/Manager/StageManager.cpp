fe053059d95c40c1fcacf4d57142d718e1b0ebed
o-takehiro <mcd2476024@stu.o-hara.ac.jp>
2025-10-12 17:07:53 +0900

    oorui
    add_Stage.cs

--- Scripts/Engine/Manager/StageManager.cpp
+++ Scripts/Engine/Manager/StageManager.cpp
@@ -1,29 +1,58 @@
 /*
- *	@file StageManager.cpp
+ *  @file StageManager.cpp
+ *  @author oorui
  */
-
-
 
 #include "StageManager.h"
-
-
+#include <DxLib.h>
 
  /*
-  *	更新
+  *  コンストラクタ
   */
-void StageManager::Update() {
+StageManager::StageManager()
+	: pEngine(nullptr)
+	, pStage(nullptr) {
 }
 
 /*
- *	描画
+ *  初期化
+ */
+void StageManager::Initialize(Engine* pEngine) {
+	this->pEngine = pEngine;
+}
+
+/*
+ *  ステージの読み込み
+ */
+void StageManager::LoadStage(const std::string& stageCsvPath) {
+	pStage = std::make_unique<Stage>();
+	pStage->Load(stageCsvPath);
+}
+
+/*
+ *  更新
+ */
+void StageManager::Update() {
+	if (pStage) {
+		pStage->Update();
+	}
+}
+
+/*
+ *  描画
  */
 void StageManager::Render() {
+	if (pStage) {
+		pStage->Render();
+	}
 }
 
 /*
- *	片付け
+ *  終了
  */
 void StageManager::Execute() {
+	if (pStage) {
+		pStage->Execute();
+		pStage.reset();
+	}
 }
-
-
--- Scripts/Engine/Manager/StageManager.h
+++ Scripts/Engine/Manager/StageManager.h
@@ -6,8 +6,12 @@
 #ifndef _STAGEMANAGER_H_
 #define _STAGEMANAGER_H_
 
+
 #include "../Singleton.h"
 #include "../Engine.h"
+#include "../Stage/Stage.h"
+#include <memory>
+#include <string>
 
  /*
   *	ステージ全体の管理
@@ -17,27 +21,46 @@ class StageManager : public Singleton<StageManager> {
 	friend class Singleton<StageManager>;
 
 private:
-	Engine* engine;
+
+	Engine* pEngine;                 // ゲームエンジン参照
+	std::unique_ptr<Stage> pStage;   // 現在のステージ *スマートポインタにより自動的に削除されやーつ
 
 
-
-private:
 	StageManager();
 	~StageManager() = default;
 
 public:
-	// 初期化
-	void Initialize(Engine* engine);
-	// 更新
+
+	/*
+	 *  初期化
+	 */
+	void Initialize(Engine* pEngine);
+
+	/*
+	 *  ステージの読み込み
+	 */
+	void LoadStage(const std::string& stageCsvPath);
+
+	/*
+	 *  更新
+	 */
 	void Update();
 
-	// 描画
+	/*
+	 *  描画
+	 */
 	void Render();
 
-	// 片付け
+	/*
+	 *  終了
+	 */
 	void Execute();
 
 
+	/*
+	 *  現在のステージの取得
+	 */
+	Stage* GetStage() const { return pStage.get(); }
 
 };
 
