6ccebb95cd0af95c24b859bc2dba05bb3c5d36c0
ichikawakuu <mcd2476004@stu.o-hara.ac.jp>
2025-10-14 15:10:52 +0900

    Update StageObjectManager

--- Scripts/Engine/Manager/StageObjectManager.h
+++ Scripts/Engine/Manager/StageObjectManager.h
@@ -7,8 +7,18 @@
 #define _STAGEOBJECTMANAGER_H_
 
 #include "../Singleton.h"
+#include "../Engine.h"
 
+/*
+ *	ステージオブジェクトの管理クラス
+ */
 class StageObjectManager : public Singleton<StageObjectManager> {
+	// フレンド宣言
+	friend class Singleton<StageObjectManager>;
+
+private:
+	Engine* enigne;
+
 
 private:
 	StageObjectManager();
