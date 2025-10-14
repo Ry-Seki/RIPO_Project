f13ae73a3d0131fc55dd49caabf5fe05f2a3b7a7
ichikawakuu <mcd2476004@stu.o-hara.ac.jp>
2025-10-14 10:10:59 +0900

    Update SpawnPoint.h

--- Scripts/Engine/Stage/StageObject/SpawnPoint.h
+++ Scripts/Engine/Stage/StageObject/SpawnPoint.h
@@ -2,3 +2,21 @@
  *	@file	SpawnPoint.h
  *  @author kuu
  */
+
+#ifndef _SPAWNPOINT_H_
+#define _SPAWNPOINT_H_
+
+#include "StageObjectBase.h"
+
+class SpawnPoint : StageObjectBase {
+private:
+
+
+public:
+	SpawnPoint() = default;
+	~SpawnPoint() = default;
+};
+
+
+
+#endif // !_SPAWNPOINT_H_
