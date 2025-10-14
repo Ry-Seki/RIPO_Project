48c68e09f78296d64f1d777a96094ab798c97fb5
ichikawakuu <mcd2476004@stu.o-hara.ac.jp>
2025-10-14 12:06:28 +0900

    Add StageObjectManager

---
+++ Scripts/Engine/Manager/StageObjectManager.h
@@ -0,0 +1,15 @@
+/*
+ *	@file	StageObjectManager.h
+ *	@author kuu
+ */
+
+#ifndef _STAGEOBJECTMANAGER_H_
+#define _STAGEOBJECTMANAGER_H_
+
+#include "../Singleton.h"
+
+class StageObjectManager : public Singleton<StageObjectManager> {
+
+};
+
+#endif // !_STAGEOBJECTMANAGER_H_
