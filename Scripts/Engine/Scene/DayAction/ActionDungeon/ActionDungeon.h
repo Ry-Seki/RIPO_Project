ac7f1a45cc7fcd279ab62b62bd252c89b4ff30aa
Ry-Seki <mcd2476094@stu.o-hara.ac.jp>
2025-10-15 13:06:12 +0900

    Seki
    Add_ActionDungeon.h

---
+++ Scripts/Engine/Scene/DayAction/ActionDungeon/ActionDungeon.h
@@ -0,0 +1,22 @@
+/*
+ *	@file	ActionDungeon.h
+ *	@author	Seki
+ */
+
+#ifndef _ACTION_DUNGEON_H_
+#define _ACTION_DUNGEON_H_
+
+#include "../DayActionBase.h"
+#include "../../../Load/LoadManager.h"
+#include "../../../Load/Model/LoadModel.h"
+#include "../../../Load/CSV/LoadCSV.h"
+
+#include <memory>
+
+/*
+ *	ダンジョンアクションクラス
+ */
+class ActionDungeon : public DayActionBase {
+
+};
+#endif // !_ACTION_DUNGEON_H_
