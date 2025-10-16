1051d40379edf9b036015ddbb94eba1f29aec762
Ry-Seki <mcd2476094@stu.o-hara.ac.jp>
2025-10-16 16:27:25 +0900

    Seki
    Fixed_ActionDungeon.cpp

--- Scripts/Engine/Scene/DayAction/ActionDungeon/ActionDungeon.cpp
+++ Scripts/Engine/Scene/DayAction/ActionDungeon/ActionDungeon.cpp
@@ -33,16 +33,6 @@ void ActionDungeon::Setup() {
         currentDungeon = dungeonList[0];
         std::cout << "[ActionDungeon] CurrentDungeon: " << currentDungeon.name
             << " Path: " << currentDungeon.dungeonPath << std::endl;
-
-        // メインスレッド逐次ロード: モデル
-        LoadManager::GetInstance().StartTask([this]() {
-            auto model = LoadManager::GetInstance().LoadResource<LoadModel>(currentDungeon.dungeonPath);
-            if (model && model->IsLoaded()) {
-                // StageManager に渡してステージ準備
-                //StageManager::GetInstance().SetStageModel(model->GetHandle());
-                std::cout << "[ActionDungeon] Model loaded and set to StageManager" << std::endl;
-            }
-        });
     }
 }
 /*
