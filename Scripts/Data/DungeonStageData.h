4bf13be12c148e1c5a58c8878660fc09f623e512
Ry-Seki <mcd2476094@stu.o-hara.ac.jp>
2025-10-27 16:39:09 +0900

    Seki
    Fixed_ActionDungeon.cpp
    Fixed_SelectionDungeon.cpp
    Fixed_DungeonStageData.h

--- Scripts/Data/DungeonStageData.h
+++ Scripts/Data/DungeonStageData.h
@@ -13,6 +13,7 @@
 
 enum class ResourceID {
     Stage1,
+    StageBone,
     Player,
     Enemy1,
     Boss,
--- Scripts/Engine/Scene/Selection/Dungeon/SelectionDungeon.cpp
+++ Scripts/Engine/Scene/Selection/Dungeon/SelectionDungeon.cpp
@@ -56,6 +56,7 @@ void SelectionDungeon::SetStageData(Engine& engine, std::shared_ptr<LoadJSON> se
 	// ダンジョンステージデータの宣言
 	DungeonStageData stageData;
 	stageData.Register(ResourceID::Stage1, dungeonData["Stage"]["StageData"]);
+	stageData.Register(ResourceID::StageBone, dungeonData["Stage"]["StageBoneData"]);
 	stageData.Register(ResourceID::Player, dungeonData["Character"]["PlayerData"]);
 	LoadManager::GetInstance().Clear();
 	ActiveDungeon(engine, stageData);
