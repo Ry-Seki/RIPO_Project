7f759beb57cd4b7d4d3d4876868683ac04313a6a
ichikawakuu <mcd2476004@stu.o-hara.ac.jp>
2025-10-28 11:45:05 +0900

    Update EnemyComponent

--- Scripts/Engine/Component/Character/EnemyComponent.cpp
+++ Scripts/Engine/Component/Character/EnemyComponent.cpp
@@ -4,12 +4,26 @@
  */
 #include "EnemyComponent.h"
 
-// コンストラクタ
-EnemyComponent::EnemyComponent() {
-
+/*
+ *	コンストラクタ
+ */
+EnemyComponent::EnemyComponent()
+	: moveSpeed(3.0f)
+{
 }
 
-// 更新処理
+/*
+ *	更新処理
+ */
 void EnemyComponent::Update(float deltaTime) {
+	GameObject* enemy = GetOwner();
+	EnemyMove(enemy, deltaTime);
+}
+
+/*
+ *	移動処理
+ */
+void EnemyComponent::EnemyMove(GameObject* enemy, float deltaTime) {
+	enemy->position.z  -= moveSpeed;
 
 }
--- Scripts/Engine/Component/Character/EnemyComponent.h
+++ Scripts/Engine/Component/Character/EnemyComponent.h
@@ -10,16 +10,29 @@
 
 class EnemyComponent : public CharacterBase {
 private:
+	// 移動スピード
+	float moveSpeed;
+	// ウェイポイント
 
 
 public:
-	// コンストラクタ
+	/*
+	 *	コンストラクタ
+	 */
 	EnemyComponent();
 	virtual ~EnemyComponent() = default;
 
 public:
-	// 更新処理
+	/*
+	 *	更新処理
+	 */
 	virtual void Update(float deltaTime) override;
+
+private:
+	/*
+	 *	移動処理
+	 */
+	void EnemyMove(GameObject* enemy, float deltaTime);
 };
 
 #endif // !_ENEMYCOMPONENT_H_
