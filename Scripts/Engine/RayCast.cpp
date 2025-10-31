b1b65ee2add201800b00193355288a88ed7c6b05
IchikawaRiku <mcd2476003@stu.o-hara.ac.jp>
2025-10-31 12:26:18 +0900

    Riku_Fix_RayCast_refactor

--- Scripts/Engine/RayCast.cpp
+++ Scripts/Engine/RayCast.cpp
@@ -5,23 +5,24 @@
 #include "RayCast.h"
 
 
- /*
-  *  レイキャスト
-  *  @param Engine* engine  
-  *  @param Vector3 startPosition   レイ開始位置
-  *  @param Vector3 direction       レイの方向
-  *  @param float   hitLength       レイが当たるまでの距離
-  */
-bool RayCast(Engine* engine, Vector3 startPosition, Vector3 direction, float& hitLength, GameObject& hitObject) {
+/*
+ *  レイキャスト
+ *  @param  Engine*         engine          
+ *  @param  Vector3         startPosition   レイ開始位置
+ *  @param  Vector3         direction       レイの方向
+ *  @param  float&          hitLength       レイが当たるまでの距離
+ *  @param  GameObject*&    hitObject       当たったオブジェクト
+ */
+bool RayCast(Engine* engine, Vector3 startPosition, Vector3 direction, float& hitLength, GameObject*& hitObject) {
     ScenePtr scene = engine->GetCurrentScene();
-    Ray ray = { startPosition, direction };
+    Ray ray = { startPosition, direction.Normalized()};
     bool hit = false;
     // 全てのオブジェクトでコライダーの座標を計算
     std::vector<Scene::WorldColliderList> colliders = scene->ChangeGameObjectWorldColliders();
 
     // ゲームオブジェクト毎の衝突判定
     for (auto box = colliders.begin(); box != colliders.end() - 1; box++) {
-        const GameObject* objBox = box->at(0).origin->GetOwner();
+        GameObject* objBox = box->at(0).origin->GetOwner();
         // 削除済みは処理しない
         if (objBox->IsDestroyed())
             continue;
@@ -35,90 +36,39 @@ bool RayCast(Engine* engine, Vector3 startPosition, Vector3 direction, float& hi
     return hit;
 }
 
- /*
-  *  RayとAABBの交差判定
-  *  @param Ray  ray   判定対象1
-  *  @param AABB box   判定対象2
-  *  @param float hitLength 当たるまでの距離
-  */
-bool RayIntersect(const Ray& ray, const AABB& box, float& hitLength, GameObject& hitObject, const GameObject* judgeObject) {
+/*
+ *  RayとAABBの交差判定
+ *  @param  Ray&            ray         判定対象1
+ *  @param  AABB&           box         判定対象2
+ *  @param  float&          hitLength   当たるまでの距離
+ *  @param  GameObject*&    hitObject   当たった最も近いオブジェクト
+ *  @param  GameObject*     checkObject 判定チェックオブジェクト
+ */
+bool RayIntersect(const Ray& ray, const AABB& box, float& hitLength, GameObject*& hitObject, GameObject* checkObject) {
     // rayのdirが0なら0のまま
-    float dx0 = 0;
-    float dx1 = 0;
-    float dy0 = 0;
-    float dy1 = 0;
-    float dz0 = 0;
-    float dz1 = 0;
-    // 0除算回避(x軸判定)
-    if (ray.dir.x == 0) {
-        // rayがx軸上でboxの左側に当たるまでの距離が0以下かつ開始位置がboxの左側より左にある場合は当たらない
-        dx0 = (box.min.x - ray.start.x) / ray.dir.x;
-        if (dx0 <= 0 && box.min.x >= ray.start.x)
-            return false;
-        // rayがx軸上でboxの右側に当たるまでの距離が0以下かつ開始位置がboxの右側より右にある場合は当たらない
-        dx1 = (box.max.x - ray.start.x) / ray.dir.x;
-        if (dx1 <= 0 && box.max.x <= ray.start.x)
-            return false;
-    }
-    else {
-        // rayがboxの左側より左にある場合は当たらない
-        if (box.min.x >= ray.start.x)
-            return false;
-        // rayがboxの右側より右にある場合は当たらない
-        if (box.max.x <= ray.start.x)
-            return false;
-    }
-
-    // 0除算回避(y軸判定)
-    if (ray.dir.y != 0) {
-        // rayがy軸上でboxの下に当たるまでの距離が0以下かつ開始位置がboxの下側より下にある場合は当たらない
-        dy0 = (box.min.y - ray.start.y) / ray.dir.y;
-        if (dy0 <= 0 && box.min.y >= ray.start.y)
-            return false;
-        // rayがy軸上でboxの上側に当たるまでの距離が0以下かつ開始位置がboxの上側より上にある場合は当たらない
-        dy1 = (box.max.y - ray.start.y) / ray.dir.y;
-        if (dy1 <= 0 && box.max.y <= ray.start.y)
-            return false;
-    }
-    else {
-        // rayがboxの下側より下にある場合は当たらない
-        if (box.min.y >= ray.start.y)
-            return false;
-        // rayがboxの上側より上にある場合は当たらない
-        if (box.max.y <= ray.start.y)
-            return false;
-    }
-
-    // 0除算回避(z軸判定)
-    if (ray.dir.z != 0) {
-        // rayがz軸上でboxの奥側に当たるまでの距離が0以下かつ開始位置がboxの奥側より奥にある場合は当たらない
-        dz0 = (box.min.z - ray.start.z) / ray.dir.z;
-        if (dz0 <= 0 && box.min.z >= ray.start.z)
-            return false;
-        // rayがz軸上でboxの手前側に当たるまでの距離が0以下かつ開始位置がboxの手前側より手前にある場合は当たらない
-        dz1 = (box.max.z - ray.start.z) / ray.dir.z;
-        if (dz1 <= 0 && box.max.z <= ray.start.z)
-            return false;
-    }
-    else {
-        // rayがboxの奥側より奥にある場合は当たらない
-        if (box.min.z >= ray.start.z)
-            return false;
-        // rayがboxの手前側より手前にある場合は当たらない
-        if (box.max.z <= ray.start.z)
-            return false;
-    }
+    float dxMin = 0;
+    float dxMax = 0;
+    float dyMin = 0;
+    float dyMax = 0;
+    float dzMin = 0;
+    float dzMax = 0;
+    // x軸の交差判定
+    IntersectCheck(box.min.x, box.max.x, ray.start.x, ray.dir.x, dxMin, dxMax);
+    // y軸の交差判定
+    IntersectCheck(box.min.y, box.max.y, ray.start.y, ray.dir.y, dyMin, dyMax);
+    // z軸の交差判定
+    IntersectCheck(box.min.z, box.max.z, ray.start.z, ray.dir.z, dzMin, dzMax);
 
     // 各軸の当たるまでの距離が最も短い長さを計算
-    Vector3 length = { dx1, dy1, dz1 };
-    if (dx0 < dx1) {
-        length.x = dx0;
+    Vector3 length = { dxMax, dyMax, dzMax };
+    if (dxMin < dxMax) {
+        length.x = dxMin;
     }
-    if (dy0 < dy1) {
-        length.y = dy0;
+    if (dyMin < dyMax) {
+        length.y = dyMin;
     }
-    if (dz0 < dz1) {
-        length.z = dz0;
+    if (dzMin < dzMax) {
+        length.z = dzMin;
     }
     
     // 全ての軸で衝突するまでの長さを保存
@@ -136,8 +86,40 @@ bool RayIntersect(const Ray& ray, const AABB& box, float& hitLength, GameObject&
     // より距離の短い方を保存
     if (hitLength > allHitLength) {
         hitLength = allHitLength;
-        hitObject = *judgeObject;
+        hitObject = checkObject;
     }
 
+    return true;
+}
+
+/*
+ *  各軸の交差判定
+ *  @param  float   boxMin  AABBの各軸の最小位置
+ *  @param  float   boxMax  AABBの各軸の最大位置
+ *  @param  float   rayPos  レイの各軸の開始位置
+ *  @param  float   rayDir  レイの各軸の方向ベクトル
+ *  @param  float&  dMin    最初に当たるまでの距離
+ *  @param  float&  dMax    最後に当たるまでの距離
+ */
+bool IntersectCheck(float boxMin, float boxMax, float rayPos, float rayDir, float& dMin, float& dMax) {
+    // 0除算回避
+    if (rayDir == 0) {
+        // rayが各軸上でboxに最初に当たるまでの距離が0以下かつ開始位置がboxの内側より外にある場合は当たらない
+        dMin = (boxMin - rayPos) / rayDir;
+        if (dMin <= 0 && boxMin >= rayPos)
+            return false;
+        // rayが各軸上でboxに最後に当たるまでの距離が0以下かつ開始位置がboxの内側より外にある場合は当たらない
+        dMax = (boxMax - rayPos) / rayDir;
+        if (dMax <= 0 && boxMax <= rayPos)
+            return false;
+    }
+    else {
+        // rayがboxの内側より外にある場合は当たらない
+        if (boxMin >= rayPos)
+            return false;
+        // rayがboxの内側より外にある場合は当たらない
+        if (boxMax <= rayPos)
+            return false;
+    }
     return true;
 }
