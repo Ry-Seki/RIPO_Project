d9b672bb55fd090c61cc6bff3a99c8fdc256ea74
ichikawakuu <mcd2476004@stu.o-hara.ac.jp>
2025-11-04 14:07:30 +0900

    Add VisionComponent.h

---
+++ Scripts/Engine/Component/Character/VisionComponent.h
@@ -0,0 +1,15 @@
+/*
+ *	@file	VisionComponent
+ *  @author kuu
+ */
+#ifndef _VISIONCOMPONENT_H_
+#define _VISIONCOMPONENT_H_
+
+#include "../Component.h"
+
+class VisionComponent : public Component {
+
+};
+
+
+#endif // !_VISIONCOMPONENT_H_
