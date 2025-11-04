d9b210f0915c52537b27c67dc131efb2fc0a04f4
ichikawakuu <mcd2476004@stu.o-hara.ac.jp>
2025-11-04 15:11:47 +0900

    Update VisionComponent

--- Scripts/Engine/Component/Character/VisionComponent.h
+++ Scripts/Engine/Component/Character/VisionComponent.h
@@ -6,8 +6,22 @@
 #define _VISIONCOMPONENT_H_
 
 #include "../Component.h"
+#include "../../RayCast.h"
 
+/*
+ *	視界判定クラス
+ */
 class VisionComponent : public Component {
+private:
+
+
+public:
+	/*
+	 *	更新処理
+	 */
+	void Update();
+
+
 
 };
 
