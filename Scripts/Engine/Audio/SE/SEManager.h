33fe6ad643b86c5c28c81af55e73907d97a8e9bf
Ry-Seki <mcd2476094@stu.o-hara.ac.jp>
2025-11-04 10:12:58 +0900

    Seki
    Fixed_SEManager.h,cpp
    Fixed_AudioSource.h,cpp
    Fixed_PlayerStatusData.json

--- Scripts/Engine/Audio/SE/SEManager.h
+++ Scripts/Engine/Audio/SE/SEManager.h
@@ -15,14 +15,16 @@
 #include <string>
 #include <list>
 #include <mutex>
+#include <unordered_map>
 
 /*
  *	SEの管理クラス
  */
 class SEManager {
 private:
+	std::unordered_map<std::string, int> seHandleMap;		// SEハンドルマップ
 	std::list<std::shared_ptr<SESource>> unuseSEList;		// 未使用のSE配列
-	std::list<std::shared_ptr<SESource>> useSEList;		// 使用中のSE配列
+	std::list<std::shared_ptr<SESource>> useSEList;			// 使用中のSE配列
 	mutable std::mutex mtx;									// スレッド安全用
 
 public:
@@ -52,15 +54,35 @@ public:
 public:
 	/*
 	 *	SE再生処理
-	 *  @param[in]	const int setHandle		再生する音源ハンドル
-	 *  @param[in]	const int setVolume		音量
+	 *  @param[in]	const std::string& setKeyName	再生するSE名
+	 *  @param[in]	const int setVolume				音量
 	 */
-	void PlaySE(const int setHandle, const int setVolume);
+	void PlaySE(const std::string& setKeyName, const int setVolume);
 	/*
 	 *	SE破棄処理
 	 *  @param[in]	std::shared_ptr<SESource> destroySE
 	 */
 	void TeardownSE(std::shared_ptr<SESource> destroySE);
+
+public:
+	/*
+	 *	SEハンドルの登録
+	 *	@param[in]	const std::string&	setKeyName	登録するSE名
+	 *	@param[in]	const int& setHandle			登録する音源ハンドル
+	 */
+	void RegisterSEHandle(const std::string& setKeyName, const int& setHandle);
+	/*
+	 *	SEハンドルが登録済みか判定
+	 *  @param[in]	const std::string& setKeyName	調べるSE名
+	 *  @return		bool
+	 */
+	bool ExistSEHandle(const std::string& setKeyName) const;
+	/*
+	 *	SEハンドルの取得
+	 *	@param[in]	const std::string& setKeyName	調べるSE名
+	 *	@return		int
+	 */
+	int GetSEHandle(const std::string& setKeyName) const;
 
 };
 	
