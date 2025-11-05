9c0c30dee4c8f08cd247767ac02db80875db44d0
ichikawakuu <mcd2476004@stu.o-hara.ac.jp>
2025-11-05 10:31:01 +0900

    Add VisionComponent.cpp

---
+++ Scripts/Engine/Component/Character/VisionComponent.cpp
@@ -0,0 +1,11 @@
+/*
+ *	@file	VisionComponent.cpp
+ *  @author kuu
+ */
+#include "VisionComponent.h"
+#include "../../RayCast.h"
+
+void VisionComponent::Update(float deltaTime)
+{
+	//RayCast(engine, )
+}
