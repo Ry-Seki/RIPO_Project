878947d589a3b10117f101dd3a8ce3fa39cdc151
o-takehiro <mcd2476024@stu.o-hara.ac.jp>
2025-11-07 12:00:57 +0900

    oorui
    Fixd_VecMath.h_Comment

--- Scripts/Engine/VecMath.h
+++ Scripts/Engine/VecMath.h
@@ -1,6 +1,7 @@
 /*
  *	@file	VecMath.h
  *	@author	Seki
+ *  @author oorui
  */
 
 #ifndef _VECMATH_H_
@@ -61,7 +62,7 @@ struct Vector3 {
 	// 演算
 	/*
 	 *	各成分毎の乗算
-	 *  @param[in]	v1	1爪のベクトル
+	 *  @param[in]	v1	1つ目のベクトル
 	 *  @param[in]	v2	2つ目のベクトル
 	 *  @return Vector3
 	 */
@@ -88,20 +89,78 @@ struct Vector3 {
 	 *  @return float
 	 */
 	float Magnitude() const;
-	// 正規化
+
+	/*
+	 *	正規化
+	 *  @return Vector3
+	 */
 	Vector3 Normalized() const;
 
+	/*
+	 *	2点間( v1 , v2 )の距離
+	 *  @param[in]	v1	1つ目のベクトル
+	 *  @param[in]	v2	2つ目のベクトル
+	 *  @return float
+	 */
 	static float Distance(const Vector3& v1, const Vector3& v2);
+
+	/*
+	 *	2点間の方向を表す単位ベクトル
+	 *  @param[in]	from	1つ目のベクトル
+	 *  @param[in]	to		2つ目のベクトル
+	 *  @return Vector3
+	 */
 	static Vector3 Direction(const Vector3& from, const Vector3& to);
+
+	/*
+	 *	正面方向のベクトル
+	 *  @param[in]	rotation	回転
+	 *  @return Vector3
+	 */
 	static Vector3 ForwardDir(const Vector3& rotation);
+
+	/*
+	 *	各成分を指定範囲内に制限する
+	 *	@param[in]  v1   対象のベクトル
+	 *	@param[in]  min  下限値ベクトル
+	 *	@param[in]  max  上限値ベクトル
+	 *  @return Vector3
+	 */
 	static Vector3 Clamp(const Vector3& v1, const Vector3& min, const Vector3& max);
+	/*
+	 *  値を指定範囲内に制限する
+	 *  @param[in]  value 対象の値
+	 *  @param[in]  min   下限値
+	 *  @param[in]  max   上限値
+	 *  @return	float
+	 */
 	static float Clamp(float value, float min, float max);
+
+	/*
+	 *	2つのベクトル間の差分を求める
+	 *	@param[in]  v1  基準となるベクトル
+	 *	@param[in]  v2  引かれるベクトル
+	 *	@return	Vector3
+	 */
 	static Vector3 Offset(const Vector3& v1, const Vector3& v2);
-	// 線形補間
+
+	/*
+	 *	線形補間
+	 *  @param[in]	v1	始点のベクトル
+	 *  @param[in]	v2	終点のベクトル
+	 *  @param[in]	t	補間係数（0.0f~1.0f）
+	 *  @return Vector3
+	 */
 	static Vector3 Lerp(const Vector3& v1, const Vector3& v2, float t);
 
-	// 球面線形補間
-	static Vector3 Slerp(const Vector3& a, const Vector3& b, float t);
+	/*
+	 *	球面線形補間
+	 *  @param[in] v1		  始点のベクトル
+	 *  @param[in] v2		  終点のベクトル
+	 *  @param[in] t		  補間係数（0.0f~1.0f）
+	 *  @return Vector3
+	 */
+	static Vector3 Slerp(const Vector3& v1, const Vector3& v2, float t);
 };
 
 /*
@@ -125,39 +184,135 @@ Vector3& operator/=(Vector3& lhs, float s);
 /*
  *	非メンバ演算関数の宣言
  */
+
+ /*
+  *	各成分毎の乗算
+  *  @param[in]	v1	1つ目のベクトル
+  *  @param[in]	v2	2つ目のベクトル
+  *  @return Vector3
+  */
 Vector3 Scale(const Vector3& v1, const Vector3& v2);
+
+/*
+ *	内積
+ *  @param[in]	v1	1つ目のベクトル
+ *  @param[in]	v2	2つ目のベクトル
+ *  @return float
+ */
 float   Dot(const Vector3& v1, const Vector3& v2);
+
+/*
+ *	外積
+ *  @param[in]	v1	1つ目のベクトル
+ *  @param[in]	v2	2つ目のベクトル
+ *  @return Vector3
+ */
 Vector3 Cross(const Vector3& v1, const Vector3& v2);
 
+/*
+ *	ベクトルの長さ
+ *  @return float
+ */
 float   Magnitude(const Vector3& v);
+
+/*
+ *	正規化
+ *  @return Vector3
+ */
 Vector3 Normalized(const Vector3& v);
+
+/*
+ *	2点間( v1 , v2 )の距離
+ *  @param[in]	v1	1つ目のベクトル
+ *  @param[in]	v2	2つ目のベクトル
+ *  @return float
+ */
 float   Distance(const Vector3& v1, const Vector3& v2);
+
+/*
+ *	2点間の方向を表す単位ベクトル
+ *  @param[in]	from	1つ目のベクトル
+ *  @param[in]	to		2つ目のベクトル
+ *  @return Vector3
+ */
 Vector3 Direction(const Vector3& from, const Vector3& to);
+
+
+/*
+ *	2つのベクトル間の差分を求める
+ *	@param[in]  v1  基準となるベクトル
+ *	@param[in]  v2  引かれるベクトル
+ *	@return	Vector3
+ */
 Vector3 Offset(const Vector3& v1, const Vector3& v2);
+
+
+/*
+ *	正面方向のベクトル
+ *  @param[in]	rotation	回転
+ *  @return Vector3
+ */
 Vector3 ForwardDir(const Vector3& rotation);
 
+/*
+ *	各成分を指定範囲内に制限する
+ *	@param[in]  v1   対象のベクトル
+ *	@param[in]  min  下限値ベクトル
+ *	@param[in]  max  上限値ベクトル
+ *  @return Vector3
+ */
 Vector3 Clamp(const Vector3& v, const Vector3& min, const Vector3& max);
+
+/*
+ *  値を指定範囲内に制限する
+ *  @param[in]  value 対象の値
+ *  @param[in]  min   下限値
+ *  @param[in]  max   上限値
+ *  @return	float
+ */
 float   Clamp(float value, float min, float max);
 
+/*
+ *	線形補間
+ *  @param[in]	v1	始点のベクトル
+ *  @param[in]	v2	終点のベクトル
+ *  @param[in]	t	補間係数（0.0f~1.0f）
+ *  @return Vector3
+ */
 Vector3 Lerp(const Vector3& v1, const Vector3& v2, float t);
+
+/*
+ *	球面線形補間
+ *  @param[in] v1		  始点のベクトル
+ *  @param[in] v2		  終点のベクトル
+ *  @param[in] t		  補間係数（0.0f~1.0f）
+ *  @return Vector3
+ */
 Vector3 Slerp(const Vector3& a, const Vector3& b, float t);
 
 /*
  *	定数ベクトル
  */
-extern const Vector3 V_ZERO;
-extern const Vector3 V_ONE;
-extern const Vector3 V_UP;
-extern const Vector3 V_DOWN;
-extern const Vector3 V_RIGHT;
-extern const Vector3 V_LEFT;
-extern const Vector3 V_FORWARD;
-extern const Vector3 V_BACK;
+extern const Vector3 V_ZERO;		  // {  0.0f, 0.0f, 0.0f }
+extern const Vector3 V_ONE;			  // {  1.0f, 1.0f, 1.0f }
+extern const Vector3 V_UP;			  // {  0.0f, 1.0f, 0.0f }
+extern const Vector3 V_DOWN;		  // {  0.0f,-1.0f, 0.0f }
+extern const Vector3 V_RIGHT;		  // {  1.0f, 0.0f, 0.0f }
+extern const Vector3 V_LEFT;		  // { -1.0f, 0.0f, 0.0f }
+extern const Vector3 V_FORWARD;		  // {  0.0f, 0.0f, 1.0f }
+extern const Vector3 V_BACK;		  // {  0.0f, 0.0f,-1.0f }
+
 
 /*
- *	DxLib VECTOR 互換関数
+ *	Vector3型からVECTOR型への型変換
+ *  @param		v	Vector3型
  */
 VECTOR ToVECTOR(const Vector3& v);
+
+/*
+ *	VECTOR型からVector3型への型変換
+ *  @param		v	VECTOR型
+ */
 Vector3 FromVECTOR(const VECTOR& v);
 
 #endif // !_VECMATH_H_
