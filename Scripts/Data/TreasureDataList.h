6277e39bf4b9a2c855cc13eb81c738a17abad338
Ry-Seki <mcd2476094@stu.o-hara.ac.jp>
2025-11-14 11:39:44 +0900

    Seki
    Add_JSON.h
    Add_AutoJSONRegistry.h
    Add_TreasureDataList.h
    Fixed_LoadJSON.h
    Sort_File

---
+++ Scripts/Data/TreasureDataList.h
@@ -0,0 +1,53 @@
+/*
+ *	@file	TreasureDataList.h
+ *	@author	Seki
+ */
+
+#ifndef _TREASURE_DATA_LIST_H_
+#define _TREASURE_DATA_LIST_H_
+
+#include "../Engine/JSON.h"
+#include "../Engine/AutoJSONRegitry.h"
+
+#include <unordered_map>
+#include <string>
+/*
+ *	お宝の価値データクラス
+ */
+class TreasureDataList {
+	std::unordered_map<std::string, int> treasureDataMap;	// お宝データのマップ
+
+public:
+	/*
+	 *	コンストラクタ
+	 */
+	TreasureDataList() = default;
+
+public:
+    /*
+     *  JSONデータの読み込み、mapに自動で登録する
+     */
+    void LoadFromJson(const JSON& setJSON) {
+        // 自動登録クラス
+        AutoJSONRegistry registry;
+        // データの登録
+        registry.LoadFromJson<int>(treasureDataMap, setJSON);
+    }
+
+public:
+	/*
+	 *  指定したお宝名の価値を取得
+	 *  @param[in]  const std::string& treasureName 検索するお宝名
+	 *  @param[out] int& value						お宝の価値
+	 *  @return bool
+	 */
+	bool TryGetValue(const std::string& treasureName, int& value) const {
+		auto it = treasureDataMap.find(treasureName);
+		if (it == treasureDataMap.end()) return false;
+
+		value = it->second;
+		return true;
+	}
+};
+
+#endif // !_TREASURE_DATA_LIST_H_
---
+++ Scripts/Engine/JSON.h
@@ -0,0 +1,18 @@
+/*
+ *	MIT License | https://github.com/nlohmann/json/releases
+ */
+
+/*
+ *	@file	JSON.h
+ *	@author	Seki
+ */
+
+#ifndef _JSON_H_
+#define _JSON_H_
+
+#include <json.hpp>
+
+// •Ê–¼’è‹`
+using JSON = nlohmann::json;
+
+#endif // !_JSON_H_
