30c0a9e2d7d3f0a233c44915846c551cb22e1284
IchikawaRiku <mcd2476003@stu.o-hara.ac.jp>
2026-01-16 17:33:44 +0900

    Riku_Add_InAction_Dungeon_StatusText

--- Scripts/Engine/Scene/GameState/InAction/Dungeon/InAction_Dungeon.cpp
+++ Scripts/Engine/Scene/GameState/InAction/Dungeon/InAction_Dungeon.cpp
@@ -136,6 +136,12 @@ void InAction_Dungeon::Render() {
 
 	}
 #endif
+	PlayerStatusData* BaseStatus = PlayerStatusManager::GetInstance().GetPlayerStatusData();
+	PlayerStatusValue playerStatus = GetPlayer()->GetComponent<PlayerComponent>()->GetPlayerStatus();
+	DrawFormatString(450, 20, GetColor(255, 255, 255), "HP : %d / HP : %d", playerStatus.HP, BaseStatus->base.HP);
+	DrawFormatString(450, 40, GetColor(255, 255, 255), "Stamina : %d / Stamina : %d", playerStatus.stamina, BaseStatus->base.stamina);
+	DrawFormatString(450, 60, GetColor(255, 255, 255), "Strength : %d / Strength : %d", playerStatus.strength, BaseStatus->base.strength);
+	DrawFormatString(450, 80, GetColor(255, 255, 255), "ResistTime : %d / ResistTime : %d", playerStatus.resistTime, BaseStatus->base.resistTime);
 }
 /*
  *	@brief	•Ğ•t‚¯ˆ—
