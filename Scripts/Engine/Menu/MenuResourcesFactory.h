89b6b281fcd36f2576071a0e9049463aa9aeb1d3
Ry-Seki <mcd2476094@stu.o-hara.ac.jp>
2026-02-03 17:31:48 +0900

    Seki
    Sort_Filter
    Add_README_MenuResources.txt
    Add_MenuREsourcesFactory.h
    Add_SaveSlotResources.json
    Fixed_Engine.cpp

---
+++ Scripts/Engine/Menu/MenuResourcesFactory.h
@@ -0,0 +1,23 @@
+/*
+ *	@file	MenuResourcesFactory.h
+ *	@author	Seki
+ */
+
+#ifndef _MENU_RESOURCES_FACTORY_H_
+#define _MENU_RESOURCES_FACTORY_H_
+
+#include "../Engine.h"
+
+/*
+ *	@brief	メニュークラスに必要なリソースを生成するクラス
+ */
+class MenuResourcesFactory {
+
+public:
+	/*
+	 *	@brief	メニュークラスに必要なリソースの生成
+	 */
+
+};
+
+#endif // !_MENU_RESOURCES_FACTORY_H_
