da023b33fbb230cf3b637c4f66059ae7f9e2dd11
ichikawakuu <mcd2476004@stu.o-hara.ac.jp>
2026-02-04 22:49:09 +0900

    Update HPBarComponentr

--- Scripts/Engine/Component/Character/HPBarComponent.cpp
+++ Scripts/Engine/Component/Character/HPBarComponent.cpp
@@ -31,18 +31,25 @@ void HPBarComponent::ShowHPBar()
 {
 	auto enemy = GetOwner();
 	VECTOR enemyPos = ToVECTOR(enemy->position);
+
+	VECTOR headPos = enemyPos;
+	headPos.y += 700 + 10.0f;
+
 	// ワールド座標からスクリーン座標に変換
-	Vector3 screenPos = DxLib::ConvWorldPosToScreenPos(enemyPos);
+	Vector3 screenPos = DxLib::ConvWorldPosToScreenPos(headPos);
 	DrawFormatString(0, 130, GetColor(255, 255, 255), "screenPos(%f,%f,%f)",
 		screenPos.x, screenPos.y, screenPos.z);
 
 	// カメラ裏なら表示しない
 	if (screenPos.z > 1.0f) return;
 
-	int BoxX = screenPos.x;
-	int BoxY = screenPos.y - 60;
+	float scale = 1.0f / screenPos.z;
+	scale = std::clamp(scale, 0.4f, 1.5f);
 
-	const float halfWidth = 40.0f;
-	const float halfHeight = 6.0f;
+	int BoxX = static_cast<int>(screenPos.x);
+	int BoxY = static_cast<int>(screenPos.y);
+
+	float halfWidth = 40.0f * scale;
+	float halfHeight = 6.0f * scale;
 	DrawBox(BoxX - halfWidth, BoxY - halfHeight, BoxX + halfWidth, BoxY + halfHeight, GetColor(0, 250, 0), true);
 }
