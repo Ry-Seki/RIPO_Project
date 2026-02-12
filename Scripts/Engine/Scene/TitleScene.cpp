24d13097139d15272d233007cddc0ea0cea75c89
o-takehiro <mcd2476024@stu.o-hara.ac.jp>
2026-02-12 14:07:51 +0900

    oorui
    Add_StageBGM

--- Scripts/Engine/Scene/TitleScene.cpp
+++ Scripts/Engine/Scene/TitleScene.cpp
@@ -36,6 +36,8 @@ void TitleScene::Initialize(Engine& engine) {
 	MenuManager::GetInstance().GetMenu<MenuConfirm>();
 	auto debugSE = LoadManager::GetInstance().LoadResource<LoadAudio>("Res/Audio/SE/ButtonPush_Debug.mp3");
 	auto goalSE = LoadManager::GetInstance().LoadResource<LoadAudio>("Res/Audio/SE/GoalSE.mp3");
+	auto dungeonBGM01 = LoadManager::GetInstance().LoadResource<LoadAudio>("Res/Audio/BGM/Dungeon/Dungeon_1/êØÇÃéç.mp3");
+	auto dungeonBGM02 = LoadManager::GetInstance().LoadResource<LoadAudio>("Res/Audio/BGM/Dungeon/Dungeon_2/ApparitionÅfs_Lullaby.mp3");
 	std::vector<std::shared_ptr<LoadSprite>> loadBGList;
 	loadBGList.push_back(LoadManager::GetInstance().LoadResource<LoadSprite>("Res/BackGround/Trealine_LoadBackground_black1.jpg"));
 	loadBGList.push_back(LoadManager::GetInstance().LoadResource<LoadSprite>("Res/BackGround/Trealine_LoadBackground_black2.jpg"));
