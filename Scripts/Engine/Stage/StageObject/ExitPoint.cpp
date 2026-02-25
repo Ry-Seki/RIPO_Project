4fc1b776538589378618b7fc94d59d2474941c82
o-takehiro <mcd2476024@stu.o-hara.ac.jp>
2026-02-25 14:48:42 +0900

    oorui
    Add_ExitStage

--- Scripts/Engine/Stage/StageObject/ExitPoint.cpp
+++ Scripts/Engine/Stage/StageObject/ExitPoint.cpp
@@ -12,14 +12,22 @@
 #include "../../Stage/StageObject/StageObjectBase.h"
 #include "../../Component/Character/CharacterUtility.h"
 #include "../../Manager/EffectManager.h"
+#include "../../Menu/System/MenuConfirm.h"
+#include "../../Menu/MenuManager.h"
+#include "../../Input/InputUtility.h"
+#include "../../Stage/StageUtility.h"
+#include "../../Fade/FadeManager.h"
 
 using namespace CharacterUtility;
+using namespace InputUtility;
+using namespace StageUtility;
 
 ExitPoint::ExitPoint()
 	: effectName("GoalEffect")
 	, viewRadius(5000.0f)
 	, exitTriger(false)
-	, pViewingEffect(nullptr) {
+	, pViewingEffect(nullptr)
+	, onTrigger(false) {
 }
 
 /*
@@ -59,22 +67,49 @@ void ExitPoint::OnCollision(const std::shared_ptr<Component>& self, const std::s
 	if (exitTriger || other->GetOwner()->name != GameConst::_CREATE_POSNAME_PLAYER)
 		return;
 
+	if (!onTrigger) {
+		onTrigger = true;
+		// プレイヤーの入力を停止
+		SetActionMapIsActive(GameEnum::ActionMap::PlayerAction, false);
+		SetActionMapIsActive(GameEnum::ActionMap::MenuAction, true);
 
+		// 確認UIを表示
+		// Menuの取得
+		auto& menu = MenuManager::GetInstance();
+		auto confirm = menu.GetMenu<MenuConfirm>();
+		confirm->SetCallback([this, &menu](GameEnum::ConfirmResult result) {
+			if (result == GameEnum::ConfirmResult::Yes) {
+				// 衝突済みにする
+				exitTriger = true;
 
-	// 衝突済みにする
-	exitTriger = true;
+				// エフェクト停止
+				pViewingEffect->EffectStop();
 
-	// エフェクト停止
-	pViewingEffect->EffectStop();
+				auto player = GetOwner()->GetComponent<PlayerComponent>();
+				if (!player)return;
+				auto treasure = player->GetOwner()->GetComponent<ArmActionComponent>()->GetLiftObject();
+				if (!treasure)return;
+				auto treasureID = treasure->GetComponent<StageObjectBase>();
+				if (!treasureID)return;
 
-	auto player = other->GetOwner()->GetComponent<PlayerComponent>();
-	if (!player)return;
-	auto treasure = player->GetOwner()->GetComponent<ArmActionComponent>()->GetLiftObject();
-	if (!treasure)return;
-	auto treasureID = treasure->GetComponent<StageObjectBase>();
-	if (!treasureID)return;
-
-	MoneyManager::GetInstance().AddTreasureMoney(treasureID->GetTreasureID());
+				MoneyManager::GetInstance().AddTreasureMoney(treasureID->GetTreasureID());
+			}
+			else if (result == GameEnum::ConfirmResult::No) {
+				// フェード
+				FadeBasePtr fadeout = FadeFactory::CreateFade(FadeType::Black, 1.0f, FadeDirection::Out, FadeMode::Stop);
+				FadeManager::GetInstance().StartFade(fadeout);
+				onTrigger = false;
+				// スタート位置に戻す
+				auto player = GetPlayer();
+				if (!player)return;
+				// 位置の設定
+				player->position = GetStartPos();
+				SetActionMapIsActive(GameEnum::ActionMap::PlayerAction, true);
+			}
+			menu.CloseTopMenu();
+			});
+		menu.OpenMenu<MenuConfirm>();
+	}
 }
 
 // モデルの読み込み
