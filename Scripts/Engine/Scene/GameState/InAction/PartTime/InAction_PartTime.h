1b254ef9d76c618e05e421f5d2b666be573ebe02
o-takehiro <mcd2476024@stu.o-hara.ac.jp>
2026-03-26 14:02:58 +0900

    oorui
    FIxd_InActionPartTIme

--- Scripts/Engine/Scene/GameState/InAction/PartTime/InAction_PartTime.h
+++ Scripts/Engine/Scene/GameState/InAction/PartTime/InAction_PartTime.h
@@ -24,12 +24,6 @@ private:
 	std::vector<MiniGamePtr> miniGameList;
 	MiniGamePtr miniGame = nullptr;
 
-	static constexpr const char* _PART_BGMPATH = "Res/Audio/BGM/Part/GameBGM.mp3";				// 倉庫番のBGMのパス
-	static constexpr const char* _PART_SE01PATH = "Res/Audio/SE/SOKOBAN/ビープ音2.mp3";			// 倉庫番の壁にあたったときのSEのパス
-	static constexpr const char* _PART_SE02PATH = "Res/Audio/SE/SOKOBAN/自動ドアが開く.mp3";	// 倉庫番の移動時のSEのパス
-	static constexpr const char* _PART_SE03PATH = "Res/Audio/SE/SOKOBAN/決定ボタンを押す7.mp3"; // 倉庫番の指定位置についたときのSEのパス
-	static constexpr const char* _SELECTPART_BGMPATH = "Res/Audio/BGM/Part/Dall_Tube_Heaven.mp3";
-
 public:
 	/*
 	 *	@brief	初期化処理
